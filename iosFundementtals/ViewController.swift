271b400dc00b7fd9a859dee57d9ad23fc39512d4
Bas Jansen <basjansen14@outlook.com>
2015-05-23 22:14:05 +0200

    Initial Commit

---
+++ iosFundementtals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosFundementtals
+//
+//  Created by Bas Jansen on 23-05-15.
+//  Copyright (c) 2015 Bas Jansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
