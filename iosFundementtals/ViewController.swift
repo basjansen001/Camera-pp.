46ca7c73e35de3f31b9bda386acabde2f1e92302
Bas Jansen <basjansenapple@gmail.com>
2015-05-24 20:57:36 +0200

    Update ViewController.swift

--- iosFundementtals/ViewController.swift
+++ iosFundementtals/ViewController.swift
@@ -11,50 +11,50 @@ import AVFoundation
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 	
-  @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
 	
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-	
-  @IBAction func useCamera(sender: AnyObject) {
-    let picker = UIImagePickerController()
-		
-    let sourceType = UIImagePickerControllerSourceType.Camera
-		
-    if UIImagePickerController.isSourceTypeAvailable(sourceType) {
-      picker.sourceType = sourceType
-			
-      let frontCamera = UIImagePickerControllerCameraDevice.Front
-			
-      if UIImagePickerController.isCameraDeviceAvailable(frontCamera) {
-        icker.cameraDevice = frontCamera
-	picker.delegate = self
-	self.presentViewController(picker, animated: true, completion: nil)
-      }
-     }
-  }
-	
-  @IBAction func openPhotoLibrary(sender: AnyObject) {
-    let picker = UIImagePickerController()
-		
-    let sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-    if UIImagePickerController.isSourceTypeAvailable(sourceType) {
-      picker.sourceType = sourceType
-      picker.delegate = self
-      self.presentViewController(picker, animated: true, completion: nil)
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-  }
 	
-  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-    let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-    self.imageView.image = image
+    @IBAction func useCamera(sender: AnyObject) {
+        let picker = UIImagePickerController()
 		
-    picker.dismissViewControllerAnimated(true, completion: nil)
-  }
+        let sourceType = UIImagePickerControllerSourceType.Camera
+		
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            picker.sourceType = sourceType
+			
+            let frontCamera = UIImagePickerControllerCameraDevice.Front
+			
+            if UIImagePickerController.isCameraDeviceAvailable(frontCamera) {
+                picker.cameraDevice = frontCamera
+	        picker.delegate = self
+	        self.presentViewController(picker, animated: true, completion: nil)
+            }
+        }
+    }
 	
-  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-    picker.dismissViewControllerAnimated(true, completion: nil)
-  }
+    @IBAction func openPhotoLibrary(sender: AnyObject) {
+        let picker = UIImagePickerController()
+		
+        let sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+	
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.imageView.image = image
+		
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+	
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
